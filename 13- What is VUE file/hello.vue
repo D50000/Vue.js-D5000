3c9238a0de90a455f3ef6a70b4940dcfd677beef
D5000 <aoe102198@gmail.com>
2019-07-17 23:05:55 +0800

    add #13 VUE file

---
+++ 13- What is VUE file/hello.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">Hello {{ who }}</div>
+</template>
+
+<script>
+module.exports = {
+  data: function() {
+    return {
+      who: 'world'
+    }
+  }
+}
+</script>
+
+// use style scoped to distinguish the same class name.
+<style scoped>
+.hello {
+  padding: .5em;
+  font-size: 2em;
+  background-color: #fcf;
+}
+</style>
